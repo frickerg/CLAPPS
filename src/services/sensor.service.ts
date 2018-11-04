b342d22f276f9f6b38c1749d202eadbff00193a7
Guillaume Fricker <fricker.g@zoho.com>
2018-11-04 14:45:02 +0100

    Reworking the sensor service and adding JS script for out-of-app testing

--- src/services/sensor.service.ts
+++ src/services/sensor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Metawear, LedPattern } from 'metawear';
+import { Metawear } from 'metawear';
 
 @Injectable({
 	providedIn: 'root'
@@ -10,15 +10,26 @@ export class SensorService {
 	constructor() {
 		this.sensor.discover(device => {
 			device.connectAndSetUp(pattern => {
-				pattern = new LedPattern();
-				this.sensor.mbl_mw_led_load_preset_pattern(pattern.ref(), pattern.LedPreset.BLINK);
-				this.sensor.mbl_mw_led_write_pattern(device.board, pattern.ref(), pattern.LedColor.GREEN);
+				pattern = new this.sensor.LedPattern();
+				this.sensor.mbl_mw_led_load_preset_pattern(pattern.ref(), this.sensor.LedPreset.BLINK);
+				this.sensor.mbl_mw_led_write_pattern(device.board, pattern.ref(), this.sensor.LedColor.GREEN);
 				this.sensor.mbl_mw_led_play(device.board);
+
+				// After 5 seconds we reset the board to clear the LED, when we receive
+				// a disconnect notice we know the reset is complete
+				setTimeout(function () {
+					device.on('disconnect', function () {
+						process.exit(0);
+					});
+					this.sensor.mbl_mw_debug_reset(device.board);
+				}, 5000);
 			}).catch(error => {
+				// TODO: Remove this after successful run
 				console.log('second level err');
 				console.log(error);
 			});
 		}).catch(error => {
+			// TODO: Remove this after successful run
 			console.log('first level err');
 			console.log(error);
 		});
