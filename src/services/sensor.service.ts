3e74a6ba4e22db5ea2e0b224589342be77b28617
Guillaume Fricker <fricker.g@zoho.com>
2018-11-15 13:52:19 +0100

    Noble update and worked on the sensor service

--- src/services/sensor.service.ts
+++ src/services/sensor.service.ts
@@ -6,32 +6,31 @@ import { Metawear } from 'metawear';
 })
 export class SensorService {
 	sensor: Metawear;
+	availableDevices: Metawear[];
 
 	constructor() {
-		this.sensor.discover(device => {
-			device.connectAndSetUp(pattern => {
-				pattern = new this.sensor.LedPattern();
-				this.sensor.mbl_mw_led_load_preset_pattern(pattern.ref(), this.sensor.LedPreset.BLINK);
-				this.sensor.mbl_mw_led_write_pattern(device.board, pattern.ref(), this.sensor.LedColor.GREEN);
-				this.sensor.mbl_mw_led_play(device.board);
+	}
 
-				// After 5 seconds we reset the board to clear the LED, when we receive
-				// a disconnect notice we know the reset is complete
-				setTimeout(function () {
-					device.on('disconnect', function () {
-						process.exit(0);
-					});
-					this.sensor.mbl_mw_debug_reset(device.board);
-				}, 5000);
-			}).catch(error => {
-				// TODO: Remove this after successful run
-				console.log('second level err');
+	public discoverDevice(): Metawear {
+		this.availableDevices = [];
+		return this.sensor.discover(device => {
+			console.log('discovered ' + device.address);
+			this.sensor.push(device);
+		});
+	}
+
+	public connectDeviceByAddress(address: string): Metawear {
+		this.sensor.discoverByAddress(address, device => {
+			console.log('discovered ' + device.address);
+			device.connectAndSetUp(device => {
+				console.log('connected ' + device.address);
+			}, error => {
 				console.log(error);
 			});
-		}).catch(error => {
-			// TODO: Remove this after successful run
-			console.log('first level err');
-			console.log(error);
 		});
+	}
+
+	public logFromDevice(address: string): Metawear {
+		//TODO
 	}
 }
