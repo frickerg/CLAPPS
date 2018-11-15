2fada3a96be2fc19d050cc31a4d7411ee0ebf473
Guillaume Fricker <fricker.g@zoho.com>
2018-11-15 14:06:01 +0100

    Implemented accelerometer stream function

--- src/services/sensor.service.ts
+++ src/services/sensor.service.ts
@@ -31,10 +31,6 @@ export class SensorService {
 		});
 	}
 
-	public logFromDevice(address: string): Metawear {
-		//TODO
-	}
-
 	public onDiscover(device) {
 		this.availableAddresses.forEach((address) => {
 			if (device.address.equalsIgnoreCase(address)) {
@@ -59,7 +55,32 @@ export class SensorService {
 	}
 
 	public startAccelStream(device) {
-		//TODO
+		device.connectAndSetUp(function (error) {
+			if (error) {
+				console.error(error);
+				process.exit(1);
+			}
+			// Set the max range of the accelerometer /TESTING!)
+			this.sensor.mbl_mw_acc_set_range(device.board, 8.0);
+			this.sensor.mbl_mw_acc_write_acceleration_config(device.board);
+			var accSignal = this.sensor.mbl_mw_acc_get_acceleration_data_signal(device.board);
+			this.sensor.mbl_mw_datasignal_subscribe(accSignal, null, this.sensor.FnVoid_VoidP_DataP.toPointer(function gotTimer(context, dataPtr) {
+				var data = dataPtr.deref();
+				var pt = data.parseValue();
+				console.log(pt.x, pt.y, pt.z);
+			}));
+			this.sensor.mbl_mw_acc_enable_acceleration_sampling(device.board);
+			this.sensor.mbl_mw_acc_start(device.board);
+
+			// Stop after 5 seconds
+			setTimeout(function () {
+				// Stop the stream
+				this.sensor.mbl_mw_acc_stop(device.board);
+				this.sensor.mbl_mw_acc_disable_acceleration_sampling(device.board);
+				this.sensor.mbl_mw_datasignal_unsubscribe(accSignal);
+				this.sensor.mbl_mw_debug_disconnect(device.board);
+			}, 5000);
+		});
 	}
 
 }
