df56ea6bb1785488dd16f20be3aca049233fe8fa
Guillaume Fricker <fricker.g@zoho.com>
2018-12-13 11:10:49 +0100

    Project reset - switching stack

--- src/pages/diary-page/diary.page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-diary',
-	templateUrl: 'diary.page.component.html',
-	styleUrls: ['diary.page.component.scss']
-})
-export class DiaryPageComponent { }
