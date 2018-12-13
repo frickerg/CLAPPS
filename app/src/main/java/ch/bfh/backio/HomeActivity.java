65c962937aba79095629df51e1f81e3170a05bc3
Guillaume Fricker <fricker.g@zoho.com>
2018-12-13 14:43:06 +0100

    Native method remains uncalled

--- app/src/main/java/ch/bfh/backio/HomeActivity.java
+++ app/src/main/java/ch/bfh/backio/HomeActivity.java
@@ -33,9 +33,11 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        /*
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        */
     }
 
     @Override
