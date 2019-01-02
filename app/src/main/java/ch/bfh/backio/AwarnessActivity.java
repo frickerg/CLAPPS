5704d98978b08d9abb36bddaa4f3834ee6adafb4
Michelle Stähli <stahm2@bfh.ch>
2019-01-02 10:53:56 +0100

    Tip, Übung und Aufklärungsseite erstellt und verknüpft

--- app/src/main/java/ch/bfh/backio/AwarnessActivity.java
+++ app/src/main/java/ch/bfh/backio/AwarnessActivity.java
@@ -7,30 +7,24 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class TipsActivity extends AppCompatActivity {
-
+public class AwarnessActivity extends AppCompatActivity {
 	private Button diaryButton;
 	private Button tipsButton;
 	private Button sensorButton;
 	private Button homeButton;
-	private Button tipButton;
-	private Button exerciseButton;
-	private Button awarnessButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_tips);
+		setContentView(R.layout.activity_awarness);
+
 
 		diaryButton = (Button) findViewById(R.id.btn_diary);
 		tipsButton = (Button) findViewById(R.id.btn_tips);
 		sensorButton = (Button) findViewById(R.id.btn_sensor);
 		homeButton = (Button) findViewById(R.id.btn_home);
-		tipButton = (Button) findViewById(R.id.btn_tip);
-		exerciseButton = (Button) findViewById(R.id.btn_exercise);
-		awarnessButton = (Button) findViewById(R.id.btn_awarness);
 
-		Context context = TipsActivity.this;
+		Context context = AwarnessActivity.this;
 		diaryButton.setOnClickListener((v -> {
 
 			Class destinationActivity = DiaryActivity.class;
@@ -41,6 +35,9 @@ public class TipsActivity extends AppCompatActivity {
 		}));
 
 		tipsButton.setOnClickListener((v -> {
+			Class tipsnActivity = TipsActivity.class;
+			Intent startTipsActivityIntent = new Intent(context, tipsnActivity);
+			startActivity(startTipsActivityIntent);
 			String message = "Button clicked!\nTipps";
 			Toast.makeText(context, message, Toast.LENGTH_LONG).show();
 		}));
@@ -58,30 +55,6 @@ public class TipsActivity extends AppCompatActivity {
 			Intent startMainActivityIntent = new Intent(context, destinationActivity);
 			startActivity(startMainActivityIntent);
 			String message = "Button clicked!\nHome";
-			Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-		}));
-
-		tipButton.setOnClickListener((v -> {
-			Class destinationActivity = TipActivity.class;
-			Intent startTipActivityIntent = new Intent(context, destinationActivity);
-			startActivity(startTipActivityIntent);
-			String message = "Button clicked!\nTip";
-			Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-		}));
-
-		exerciseButton.setOnClickListener((v -> {
-			Class destinationActivity = ExerciseActivity.class;
-			Intent startExerciseActivityIntent = new Intent(context, destinationActivity);
-			startActivity(startExerciseActivityIntent);
-			String message = "Button clicked!\nÜbungen";
-			Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-		}));
-
-		awarnessButton.setOnClickListener((v -> {
-			Class destinationActivity = AwarnessActivity.class;
-			Intent startAwarnessActivityIntent = new Intent(context, destinationActivity);
-			startActivity(startAwarnessActivityIntent);
-			String message = "Button clicked!\nAufklärung";
 			Toast.makeText(context, message, Toast.LENGTH_LONG).show();
 		}));
 	}
