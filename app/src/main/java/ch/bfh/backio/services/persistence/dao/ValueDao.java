56c1bb190caacca909aba41d20316f1b02f224af
Guillaume Fricker <fricker.g@zoho.com>
2019-01-13 17:59:52 +0100

    Updated JavaDoc coverage

--- app/src/main/java/ch/bfh/backio/services/persistence/dao/ValueDao.java
+++ app/src/main/java/ch/bfh/backio/services/persistence/dao/ValueDao.java
@@ -8,18 +8,42 @@ import ch.bfh.backio.services.persistence.entity.Value;
 
 import java.util.List;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface ValueDao.
+ */
 @Dao
 public interface ValueDao {
 
+	/**
+	 * Gets the all.
+	 *
+	 * @return the all
+	 */
 	@Query("SELECT * FROM value")
 	List<Value> getAll();
 
+	/**
+	 * Count values.
+	 *
+	 * @return the int
+	 */
 	@Query("SELECT COUNT(*) from value")
 	int countValues();
 
+	/**
+	 * Insert all.
+	 *
+	 * @param values the values
+	 */
 	@Insert
 	void insertAll(Value... values);
 
+	/**
+	 * Delete.
+	 *
+	 * @param value the value
+	 */
 	@Delete
 	void delete(Value value);
 }
